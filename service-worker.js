8f54f7b282cae995b16acaebf0972525c9869b11
zhangyuchen2000-prog <zhangyuchen2000@gmail.com>
2026-07-13 20:00:21 +1200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,20 @@
-const CACHE_NAME = "daily-habit-tracker-v1";
+const CACHE_NAME = "daily-habit-tracker-v2";
 const ASSETS = ["./", "index.html", "manifest.json", "icon.svg"];
 
 self.addEventListener("install", event => {
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
   );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)))
+    )
+  );
+  self.clients.claim();
 });
 
 self.addEventListener("fetch", event => {
