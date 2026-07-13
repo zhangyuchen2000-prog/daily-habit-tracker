f0c6dd8d47f0e881693543acb513d0e21f01d35d
zhangyuchen2000-prog <zhangyuchen2000@gmail.com>
2026-07-13 19:48:15 +1200

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,14 @@
+const CACHE_NAME = "daily-habit-tracker-v1";
+const ASSETS = ["./", "index.html", "manifest.json", "icon.svg"];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
